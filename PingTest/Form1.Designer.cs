395ed2b522ebe966e2c802dc200fcac8126287a0
Liming Jin <jinliming2@gmail.com>
2017-01-13 20:47:51 +0800

    First version.👻

--- PingTest/Form1.Designer.cs
+++ PingTest/Form1.Designer.cs
@@ -23,12 +23,310 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            "FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF",
+            "Net"}, -1);
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            System.Windows.Forms.ColumnHeader columnHeader2;
+            System.Windows.Forms.ColumnHeader columnHeader3;
+            System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            this.lstIp = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.NumericUpDown();
+            this.txtTimeout = new System.Windows.Forms.NumericUpDown();
+            this.labTip = new System.Windows.Forms.Label();
+            this.txtThread = new System.Windows.Forms.NumericUpDown();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            tableLayoutPanel1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTimeout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtThread)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(this.lstIp, 0, 0);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 1);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(12);
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(1008, 605);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lstIp
+            // 
+            this.lstIp.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            columnHeader2,
+            columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lstIp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstIp.FullRowSelect = true;
+            this.lstIp.GridLines = true;
+            this.lstIp.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem6});
+            this.lstIp.Location = new System.Drawing.Point(15, 15);
+            this.lstIp.Name = "lstIp";
+            this.lstIp.ShowGroups = false;
+            this.lstIp.ShowItemToolTips = true;
+            this.lstIp.Size = new System.Drawing.Size(978, 540);
+            this.lstIp.TabIndex = 1;
+            this.lstIp.UseCompatibleStateImageBehavior = false;
+            this.lstIp.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "IP";
+            columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Comment";
+            columnHeader2.Width = 430;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Ping";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "TTL";
+            this.columnHeader4.Width = 40;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Information";
+            this.columnHeader5.Width = 120;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.Controls.Add(this.btnAdd);
+            flowLayoutPanel1.Controls.Add(this.btnDelete);
+            flowLayoutPanel1.Controls.Add(this.btnStart);
+            flowLayoutPanel1.Controls.Add(this.txtThread);
+            flowLayoutPanel1.Controls.Add(label3);
+            flowLayoutPanel1.Controls.Add(this.txtNum);
+            flowLayoutPanel1.Controls.Add(label1);
+            flowLayoutPanel1.Controls.Add(this.txtTimeout);
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Controls.Add(this.labTip);
+            flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            flowLayoutPanel1.Location = new System.Drawing.Point(15, 561);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new System.Drawing.Size(978, 29);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(900, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "重载";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(819, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "删除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(738, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(609, 3);
+            this.txtNum.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.txtNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(36, 21);
+            this.txtNum.TabIndex = 4;
+            this.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNum.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(562, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(41, 29);
+            label1.TabIndex = 6;
+            label1.Text = "次数：";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTimeout
+            // 
+            this.txtTimeout.Increment = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.txtTimeout.Location = new System.Drawing.Point(503, 3);
+            this.txtTimeout.Maximum = new decimal(new int[] {
+            30000,
+            0,
+            0,
+            0});
+            this.txtTimeout.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.txtTimeout.Name = "txtTimeout";
+            this.txtTimeout.Size = new System.Drawing.Size(53, 21);
+            this.txtTimeout.TabIndex = 8;
+            this.txtTimeout.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTimeout.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(438, 0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(59, 29);
+            label2.TabIndex = 7;
+            label2.Text = "|||超时：";
+            label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labTip
+            // 
+            this.labTip.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labTip.AutoSize = true;
+            this.labTip.Location = new System.Drawing.Point(432, 0);
+            this.labTip.Name = "labTip";
+            this.labTip.Size = new System.Drawing.Size(0, 29);
+            this.labTip.TabIndex = 5;
+            this.labTip.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(651, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(41, 29);
+            label3.TabIndex = 9;
+            label3.Text = "线程：";
+            label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtThread
+            // 
+            this.txtThread.Location = new System.Drawing.Point(698, 3);
+            this.txtThread.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.txtThread.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtThread.Name = "txtThread";
+            this.txtThread.Size = new System.Drawing.Size(34, 21);
+            this.txtThread.TabIndex = 10;
+            this.txtThread.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtThread.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1008, 605);
+            this.Controls.Add(tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTimeout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtThread)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListView lstIp;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown txtNum;
+        private System.Windows.Forms.Label labTip;
+        private System.Windows.Forms.NumericUpDown txtTimeout;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.NumericUpDown txtThread;
     }
 }
 
--- PingTest/Form1.cs
+++ PingTest/Form1.cs
@@ -1,17 +1,207 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PingTest {
     public partial class Form1 : Form {
+        private delegate void AddListItem(ListViewItem item);
+        private delegate void ControlDelegate(bool privilege, string tip);
+        private delegate void Tip(string tip);
+        private delegate string[] GetItem();
+        private delegate void SetItem(int index, double ping, double TTL, string information);
+        private ControlDelegate control;
+        private Tip tip;
+        private GetItem getItem;
+        private SetItem setItem;
+        private static int number;
+        private static int timeout;
+        private static int index = 0;
+        private static bool stop = false;
         public Form1() {
             InitializeComponent();
+            control = new ControlDelegate(Control);
+            tip = new Tip((string t) => {
+                labTip.Text = t;
+                labTip.Update();
+            });
+            getItem = new GetItem(() => {
+                if(stop || index >= lstIp.Items.Count) {
+                    return null;
+                }
+                string[] r;
+                if(lstIp.Items[index].SubItems.Count > 1) {
+                    r =  new string[] {
+                        index.ToString(),
+                        lstIp.Items[index].SubItems[0].Text,
+                        lstIp.Items[index].SubItems[1].Text
+                    };
+                } else {
+                    r = new string[] { lstIp.Items[index].Text, "" };
+                }
+                ++index;
+                return r;
+            });
+            setItem = new SetItem((int index, double ping, double TTL, string information) => {
+                if(stop) {
+                    return;
+                }
+                string info = "";
+                if(lstIp.Items[index].SubItems.Count >= 5) {
+                    info = lstIp.Items[index].SubItems[4].Text + " ";
+                }
+                info = (info + information).Trim();
+                while(lstIp.Items[index].SubItems.Count > 2) {
+                    lstIp.Items[index].SubItems.RemoveAt(2);
+                }
+                lstIp.Items[index].SubItems.Add(double.IsNaN(ping) ? "--" : Math.Round(ping, 3).ToString());
+                lstIp.Items[index].SubItems.Add(double.IsNaN(TTL) ? "--" : ((int)TTL).ToString());
+                lstIp.Items[index].SubItems.Add(info);
+            });
+        }
+        private void Control(bool privilege, string tip) {
+            btnAdd.Enabled = privilege;
+            btnDelete.Enabled = privilege;
+            btnStart.Enabled = privilege;
+            txtNum.Enabled = privilege;
+            txtTimeout.Enabled = privilege;
+            txtThread.Enabled = privilege;
+            labTip.Text = tip;
+            labTip.Update();
+        }
+        private static string filename = ".\\ips.txt";
+        private void Form1_Load(object sender, EventArgs e) {
+            if(!File.Exists(filename)) {
+                StreamWriter sw = File.CreateText(filename);
+                sw.WriteLine("# The lines that begin with # are comments.");
+                sw.WriteLine("# Input IP line by line, format: ip Comment.");
+                sw.WriteLine("# Support IPv4 and IPv6, Whether or not to support ipv6 depends on your network.");
+                sw.WriteLine();
+                sw.WriteLine("127.0.0.1 Localhost loopback");
+                sw.WriteLine("www.google.com Google, Domain name will be resolved to ip address.");
+                sw.Close();
+                sw.Dispose();
+            }
+            LoadIPs();
+        }
+        private void LoadIPs() {
+            Control(false, "正在加载 IP 列表……");
+            lstIp.Items.Clear();
+            string[] ips = File.ReadAllLines(filename);
+            AddListItem addListItem = new AddListItem((ListViewItem item) => {
+                lstIp.Items.Add(item);
+            });
+            new Thread(() => {
+                foreach(string line in ips) {
+                    string l = line.Trim();
+                    if(l.StartsWith("#")) {
+                        continue;
+                    }
+                    string[] i = l.Split(new char[] { ' ', '\t' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                    if(i.Length < 1) {
+                        continue;
+                    }
+                    IPAddress ip;
+                    if(!IPAddress.TryParse(i[0], out ip)) {
+                        try {
+                            IPHostEntry iphe = Dns.GetHostEntry(i[0]);
+                            if(iphe.AddressList.Length > 0) {
+                                ip = iphe.AddressList[0];
+                            } else {
+                                continue;
+                            }
+                        } catch {
+                            continue;
+                        }
+                    }
+                    ListViewItem lvi;
+                    if(i.Length > 1) {
+                        lvi = new ListViewItem(new string[] { ip.ToString(), i[1] });
+                    } else {
+                        lvi = new ListViewItem(new string[] { ip.ToString(), "" });
+                    }
+                    lvi.ToolTipText = i[0];
+                    lstIp.Invoke(addListItem, lvi);
+                }
+                Invoke(control, true, "IP 列表加载完毕！");
+            }).Start();
+        }
+        private void btnDelete_Click(object sender, EventArgs e) {
+            if(MessageBox.Show("确定删除？", "删除", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.OK) {
+                Control(false, "正在删除……");
+                string[] ips = File.ReadAllLines(filename);
+                string DELETE_FLAG = "----------";
+                foreach(ListViewItem lvi in lstIp.SelectedItems) {
+                    for(int i = 0; i < ips.Length; ++i) {
+                        if(ips[i].Length > 0 && ips[i][0] != '-' && ips[i].TrimStart().StartsWith(lvi.ToolTipText)) {
+                            ips[i] = DELETE_FLAG;
+                        }
+                    }
+                    lstIp.Items.Remove(lvi);
+                }
+                StreamWriter sw = new StreamWriter(filename, false);
+                foreach(string ip in ips) {
+                    if(ip == DELETE_FLAG) {
+                        continue;
+                    }
+                    sw.WriteLine(ip);
+                }
+                sw.Close();
+                sw.Dispose();
+                Control(true, "删除成功！");
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e) {
+            Form1_Load(sender, e);
+        }
+        private void btnStart_Click(object sender, EventArgs e) {
+            stop = false;
+            Control(false, "正在 Ping ……");
+            number = (int)txtNum.Value;
+            timeout = (int)txtTimeout.Value;
+            index = 0;
+            int threads = (int)txtThread.Value;
+            Thread[] pool = new Thread[threads];
+            for(int i = 0; i < threads; ++i) {
+                pool[i] = new Thread(runTest);
+                pool[i].Start();
+            }
+            new Thread(() => {
+                for(int i = 0; i < threads; ++i) {
+                    pool[i].Join();
+                }
+                Invoke(control, true, "所有任务完成！");
+            }).Start();
+        }
+        private void runTest() {
+            Ping ping = new Ping();
+            PingReply reply;
+            while(true) {
+                string[] ip = Invoke(getItem) as string[];
+                if(ip == null) {
+                    break;
+                }
+                Invoke(tip, string.Format("[1]正在 Ping {0}, {1} ……", ip[1], ip[2]));
+                long total = 0;
+                int ttl = 0;
+                int failed = 0;
+                for(int j = 1; j <= number; ++j) {
+                    reply = ping.Send(ip[1], timeout);
+                    if(reply.Address != null) {
+                        total += reply.RoundtripTime;
+                        ttl += reply.Options != null ? reply.Options.Ttl : 0;
+                        Invoke(tip, string.Format("[{0}]{1}ms, {2}Bytes, TTL: {3}, {4}, {5}", j, reply.RoundtripTime, reply.Buffer.Length, reply.Options != null ? reply.Options.Ttl.ToString() : "--", reply.Address, ip[2]));
+                        Invoke(setItem, int.Parse(ip[0]), total / (double)(j - failed), ttl / (double)(j - failed), "");
+                    } else {
+                        ++failed;
+                        Invoke(tip, string.Format("[{0}]{1}, {2}, {3}", j, reply.Status.ToString(), ip[1], ip[2]));
+                        Invoke(setItem, int.Parse(ip[0]), total / (double)(j - failed), ttl / (double)(j - failed), reply.Status.ToString());
+                    }
+                }
+            }
         }
     }
 }
